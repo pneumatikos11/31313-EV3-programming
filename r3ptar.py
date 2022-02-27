729f6730fe02d1d3abcb120b174c9d1c9847925e
pneumatikos11 <76041325+pneumatikos11@users.noreply.github.com>
2022-02-27 21:11:46 +0000

    Code for the snake build

---
+++ r3ptar.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class R3ptar:
+    
+
+    def __init__(
+            self,
+            steering_motor_port: Port = Port.A,
+            driving_motor_port: Port = Port.B,
+            striking_motor_port: Port = Port.D,
+            touch_sensor_port: Port = Port.S1,
+            ir_sensor_port: Port = Port.S4,
+            ir_beacon_channel: int = 1):
+        self.ev3_brick = EV3Brick()
+
+        self.steering_motor = Motor(port=steering_motor_port,
+                                    positive_direction=Direction.CLOCKWISE)
+        self.driving_motor = Motor(port=driving_motor_port,
+                                   positive_direction=Direction.CLOCKWISE)
+        self.striking_motor = Motor(port=striking_motor_port,
+                                    positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+
+    def drive_by_ir_beacon(
+            self,
+            speed: float = 1000,    # mm/s
+            ):
+        ir_beacons_pressed = \
+            set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+
+        if ir_beacons_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+            self.driving_motor.run(speed=speed)
+
+        elif ir_beacons_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+            self.driving_motor.run(speed=-speed)
+
+        elif ir_beacons_pressed == {Button.LEFT_UP}:
+            self.steering_motor.run(speed=-500)
+            self.driving_motor.run(speed=speed)
+
+        elif ir_beacons_pressed == {Button.RIGHT_UP}:
+            self.steering_motor.run(speed=500)
+            self.driving_motor.run(speed=speed)
+
+        elif ir_beacons_pressed == {Button.LEFT_DOWN}:
+            self.steering_motor.run(speed=-500)
+            self.driving_motor.run(speed=-speed)
+
+        elif ir_beacons_pressed == {Button.RIGHT_DOWN}:
+            self.steering_motor.run(speed=500)
+            self.driving_motor.run(speed=-speed)
+
+        else:
+            self.steering_motor.hold()
+            self.driving_motor.stop()
+
+    def strike_by_ir_beacon(self, speed: float = 1000):
+        if Button.BEACON in \
+                self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+            self.striking_motor.run_time(
+                speed=speed,
+                time=1000,
+                then=Stop.COAST,
+                wait=True)
+
+            self.striking_motor.run_time(
+                speed=-speed,
+                time=1000,
+                then=Stop.COAST,
+                wait=True)
+
+            while Button.BEACON in \
+                    self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                pass
+
+    def hiss_if_touched(self):
+        if self.touch_sensor.pressed():
+            self.ev3_brick.speaker.play_file(file=SoundFile.SNAKE_RATTLE)
+    
+    def button_movement(self):
+        if self.ev3brick.buttons.pressed() == LEFT:
+            
+
